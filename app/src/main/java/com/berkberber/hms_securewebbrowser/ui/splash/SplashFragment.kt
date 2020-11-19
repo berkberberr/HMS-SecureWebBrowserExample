1706f4cb0eca75fcdd266707e53d8fa742cb8553
unknown <berkberberr@gmail.com>
2020-11-19 23:09:17 +0300

    Splash screen has been designed

--- app/src/main/java/com/berkberber/hms_securewebbrowser/ui/splash/SplashFragment.kt
+++ app/src/main/java/com/berkberber/hms_securewebbrowser/ui/splash/SplashFragment.kt
@@ -1,6 +1,20 @@
 package com.berkberber.hms_securewebbrowser.ui.splash
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import com.berkberber.hms_securewebbrowser.R
+import com.berkberber.hms_securewebbrowser.databinding.FragmentSplashBinding
 
 class SplashFragment: Fragment() {
+    private lateinit var binding: FragmentSplashBinding
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_splash, container, false)
+
+        return binding.root
+    }
 }

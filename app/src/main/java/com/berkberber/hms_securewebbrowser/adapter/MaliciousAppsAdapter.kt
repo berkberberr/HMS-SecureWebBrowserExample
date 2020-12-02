1d35462da16409456e6abfb959f83e6f3c85ca41
unknown <berkberberr@gmail.com>
2020-12-02 19:50:03 +0300

    Removing malicious app from device

--- app/src/main/java/com/berkberber/hms_securewebbrowser/adapter/MaliciousAppsAdapter.kt
+++ app/src/main/java/com/berkberber/hms_securewebbrowser/adapter/MaliciousAppsAdapter.kt
@@ -5,9 +5,10 @@ import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.berkberber.hms_securewebbrowser.data.model.MaliciousApps
 import com.berkberber.hms_securewebbrowser.databinding.ItemMaliciousAppBinding
+import com.berkberber.hms_securewebbrowser.ui.malicious.DeleteClickListener
 import org.koin.core.component.KoinComponent
 
-class MaliciousAppsAdapter(private var maliciousAppList: List<MaliciousApps>):
+class MaliciousAppsAdapter(private var maliciousAppList: List<MaliciousApps>, private val deleteClickListener: DeleteClickListener):
     RecyclerView.Adapter<MaliciousAppsAdapter.MaliciousAppViewHolder>(), KoinComponent{
 
     private lateinit var binding: ItemMaliciousAppBinding
@@ -20,11 +21,15 @@ class MaliciousAppsAdapter(private var maliciousAppList: List<MaliciousApps>):
     override fun onBindViewHolder(holder: MaliciousAppViewHolder, position: Int) {
         var maliciousApp = maliciousAppList[position]
         holder.bindMaliciousApp(maliciousApp)
+
+        holder.binding.maliciousAppRoot.setOnClickListener {
+            deleteClickListener.selectedApp(maliciousApp)
+        }
     }
 
     override fun getItemCount(): Int = maliciousAppList.size
 
-    inner class MaliciousAppViewHolder(private val binding: ItemMaliciousAppBinding):
+    inner class MaliciousAppViewHolder(val binding: ItemMaliciousAppBinding):
         RecyclerView.ViewHolder(binding.root){
 
         fun bindMaliciousApp(maliciousApp: MaliciousApps){
--- app/src/main/java/com/berkberber/hms_securewebbrowser/ui/malicious/MaliciousFragment.kt
+++ app/src/main/java/com/berkberber/hms_securewebbrowser/ui/malicious/MaliciousFragment.kt
@@ -1,10 +1,16 @@
 package com.berkberber.hms_securewebbrowser.ui.malicious
 
+import android.app.Activity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.berkberber.hms_securewebbrowser.R
 import com.berkberber.hms_securewebbrowser.adapter.MaliciousAppsAdapter
 import com.berkberber.hms_securewebbrowser.data.model.ErrorItem
 import com.berkberber.hms_securewebbrowser.data.model.MaliciousApps
@@ -14,6 +20,10 @@ import com.berkberber.hms_securewebbrowser.ui.base.BaseViewModel
 import com.berkberber.hms_securewebbrowser.utils.Constants
 
 class MaliciousFragment: BaseFragment<BaseViewModel, FragmentMaliciousBinding>(){
+    private lateinit var maliciousApps: MutableList<MaliciousApps>
+    private val DELETE_REQUEST_CODE = 1001
+
+    private var selectedMaliciousApp: MaliciousApps? = null
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -24,9 +34,39 @@ class MaliciousFragment: BaseFragment<BaseViewModel, FragmentMaliciousBinding>()
         arguments?.let { arguments ->
             binding.errorItem = arguments.getSerializable(Constants.ERROR_ITEM) as ErrorItem?
 
-            var maliciousApps = arguments.getSerializable(Constants.MALICIOUS_APPS) as List<MaliciousApps>
-            setRecyclerView(maliciousApps)
+            maliciousApps = arguments.getSerializable(Constants.MALICIOUS_APPS) as MutableList<MaliciousApps>
+            setRecyclerView()
         }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == DELETE_REQUEST_CODE){
+            when(resultCode){
+                Activity.RESULT_OK -> {
+                    maliciousApps.remove(selectedMaliciousApp)
+                    setRecyclerView()
+                    selectedMaliciousApp = null
+                }
+                Activity.RESULT_CANCELED -> {
+                    Toast.makeText(requireContext(), requireContext().getString(R.string.should_delete_app), Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    private var deleteClickListener = object: DeleteClickListener{
+        override fun selectedApp(maliciousApp: MaliciousApps) {
+            selectedMaliciousApp = maliciousApp
+
+            var deleteIntent = Intent(Intent.ACTION_DELETE).apply {
+                data = Uri.parse("package:${maliciousApp.packageName}")
+                putExtra(Intent.EXTRA_RETURN_RESULT, true)
+            }
+            startActivityForResult(deleteIntent, DELETE_REQUEST_CODE)
+        }
+
     }
 
     override fun getViewModel(): BaseViewModel = BaseViewModel()
@@ -35,8 +75,16 @@ class MaliciousFragment: BaseFragment<BaseViewModel, FragmentMaliciousBinding>()
         return FragmentMaliciousBinding.inflate(inflater, container, false)
     }
 
-    private fun setRecyclerView(maliciousApps: List<MaliciousApps>){
-        binding.maliciousAppsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
-        binding.maliciousAppsRecyclerView.adapter = MaliciousAppsAdapter(maliciousApps)
+    private fun setRecyclerView(){
+        if(maliciousApps.size > 0){
+            binding.maliciousAppsRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+            binding.maliciousAppsRecyclerView.adapter = MaliciousAppsAdapter(maliciousApps, deleteClickListener)
+        } else{
+            findNavController().navigate(R.id.action_maliciousFragment_to_browserFragment)
+        }
     }
+}
+
+interface DeleteClickListener{
+    fun selectedApp(maliciousApps: MaliciousApps)
 }
